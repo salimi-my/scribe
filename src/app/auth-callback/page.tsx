a867d0e21566b2d0e63860463a3bbc3db5207f15
Mohamad Salimi <contact@salimi.my>
2023-10-07 01:41:42 +0800

    Setup trpc

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,10 +1,21 @@
 import { useRouter, useSearchParams } from 'next/navigation';
 
+import { trpc } from '@/app/_trpc/client';
+
 const AuthCallbackPage = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
 
   const origin = searchParams.get('origin');
+
+  const { data } = trpc.authCallback.useQuery(undefined, {
+    onSuccess: ({ success }) => {
+      if (success) {
+        // User is synced to db
+        router.push(origin ? `/${origin}` : '/dashboard');
+      }
+    }
+  });
 
   return <div>AuthCallbackPage</div>;
 };
