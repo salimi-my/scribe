9098828b57ea4b68d93892ad63eeaa14198f5cb3
Mohamad Salimi <contact@salimi.my>
2023-10-07 17:23:05 +0800

    Setup database

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google';
 
 import { cn } from '@/lib/utils';
 import Navbar from '@/components/Navbar';
+import Providers from '@/components/Providers';
 
 import './globals.css';
 
@@ -20,15 +21,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body
-        className={cn(
-          'min-h-screen font-sans antialiased grainy',
-          inter.className
-        )}
-      >
-        <Navbar />
-        {children}
-      </body>
+      <Providers>
+        <body
+          className={cn(
+            'min-h-screen font-sans antialiased grainy',
+            inter.className
+          )}
+        >
+          <Navbar />
+          {children}
+        </body>
+      </Providers>
     </html>
   );
 }
--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,16 +1,32 @@
 import { TRPCError } from '@trpc/server';
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 
+import { db } from '@/db';
 import { publicProcedure, router } from '@/trpc/trpc';
 
 export const appRouter = router({
-  authCallback: publicProcedure.query(() => {
+  authCallback: publicProcedure.query(async () => {
     const { getUser } = getKindeServerSession();
     const user = getUser();
 
     if (!user.id || !user.email) throw new TRPCError({ code: 'UNAUTHORIZED' });
 
     // Check if the user exist in db
+    const dbUser = await db.user.findFirst({
+      where: {
+        id: user.id
+      }
+    });
+
+    if (!dbUser) {
+      // Create user in db
+      await db.user.create({
+        data: {
+          id: user.id,
+          email: user.email
+        }
+      });
+    }
 
     return { success: true };
   })
