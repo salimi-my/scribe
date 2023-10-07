0388cf7cd4e94b989063a119952194f4974f5488
Mohamad Salimi <contact@salimi.my>
2023-10-08 01:02:24 +0800

    Create PdfRenderer & ChatWrapper components

--- src/app/dashboard/[fileId]/page.tsx
+++ src/app/dashboard/[fileId]/page.tsx
@@ -2,6 +2,8 @@ import { notFound, redirect } from 'next/navigation';
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 
 import { db } from '@/db';
+import PdfRenderer from '@/components/PdfRenderer';
+import ChatWrapper from '@/components/ChatWrapper';
 
 interface FilePageProps {
   params: {
@@ -30,7 +32,21 @@ const FilePage = async ({ params }: FilePageProps) => {
     notFound();
   }
 
-  return <div>{fileId}</div>;
+  return (
+    <div className='flex-1 justify-between flex flex-col h-[calc(100vh-3.5rem)]'>
+      <div className='mx-auto w-full max-w-8xl grow lg:flex xl:px-2'>
+        <div className='flex-1 xl:flex'>
+          <div className='px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6'>
+            <PdfRenderer />
+          </div>
+        </div>
+
+        <div className='shrink-0 flex-[0.75] border-t border-gray-200 lg:w-96 lg:border-l lg:border-t-0'>
+          <ChatWrapper />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default FilePage;
