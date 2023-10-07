bb80ebb09707f92b6ade9c6a4207a537d71ce9b0
Mohamad Salimi <contact@salimi.my>
2023-10-07 17:26:04 +0800

    Finishing up user db sync

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,24 @@
 import { redirect } from 'next/navigation';
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+import { db } from '@/db';
 
-const DashboardPage = () => {
+const DashboardPage = async () => {
   const { getUser } = getKindeServerSession();
   const user = getUser();
 
-  if (!user || !user.id) redirect('/auth-callback?origin=dashboard');
+  if (!user || !user.id) {
+    redirect('/auth-callback?origin=dashboard');
+  }
+
+  const dbUser = await db.user.findFirst({
+    where: {
+      id: user.id
+    }
+  });
+
+  if (!dbUser) {
+    redirect('/auth-callback?origin=dashboard');
+  }
 
   return <div>{user.email}</div>;
 };
