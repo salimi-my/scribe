5caabf398833c33daa9ec09e9b95faee7f7565ee
Mohamad Salimi <contact@salimi.my>
2023-10-10 21:36:15 +0800

    Pass file id to chatwrapper

--- src/app/dashboard/[fileId]/page.tsx
+++ src/app/dashboard/[fileId]/page.tsx
@@ -3,7 +3,7 @@ import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 
 import { db } from '@/db';
 import PdfRenderer from '@/components/PdfRenderer';
-import ChatWrapper from '@/components/ChatWrapper';
+import ChatWrapper from '@/components/chat/ChatWrapper';
 
 interface FilePageProps {
   params: {
@@ -42,7 +42,7 @@ const FilePage = async ({ params }: FilePageProps) => {
         </div>
 
         <div className='shrink-0 flex-[0.75] border-t border-gray-200 lg:w-96 lg:border-l lg:border-t-0'>
-          <ChatWrapper />
+          <ChatWrapper fileId={file.id} />
         </div>
       </div>
     </div>
