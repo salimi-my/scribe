da6b67dd98e980bc43cdc5c2a795b889c1411180
Mohamad Salimi <contact@salimi.my>
2023-10-13 23:01:29 +0800

    Update billing form

--- src/app/dashboard/billing/page.tsx
+++ src/app/dashboard/billing/page.tsx
@@ -1,5 +1,10 @@
-const BillingPage = () => {
-  return <div>BillingPage</div>;
+import BillingForm from '@/components/BillingForm';
+import { getUserSubscriptionPlan } from '@/lib/stripe';
+
+const BillingPage = async () => {
+  const subscriptionPlan = await getUserSubscriptionPlan();
+
+  return <BillingForm subscriptionPlan={subscriptionPlan} />;
 };
 
 export default BillingPage;
