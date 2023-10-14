935e983aae7aa6f5958bd7f3a6880c8978d264d9
Mohamad Salimi <contact@salimi.my>
2023-10-14 15:19:56 +0800

    Update metadata

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,6 @@
-import { type ClassValue, clsx } from 'clsx';
+import { Metadata } from 'next';
 import { twMerge } from 'tailwind-merge';
+import { type ClassValue, clsx } from 'clsx';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -9,4 +10,48 @@ export function absoluteUrl(path: string) {
   if (typeof window !== 'undefined') return path;
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}${path}`;
   return `http://localhost:${process.env.PORT ?? 3000}${path}`;
+}
+
+export function constructMetadata({
+  title = 'Scribe — Chat with Your Documents',
+  description = 'Enhance your document experience with Scribe: Upload PDFs and chat with them through AI for seamless productivity. Unlock the power of interactive PDFs today!',
+  image = '/thumbnail.png',
+  icons = '/favicon.ico',
+  noIndex = false
+}: {
+  title?: string;
+  description?: string;
+  image?: string;
+  icons?: string;
+  noIndex?: boolean;
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [
+        {
+          url: image
+        }
+      ]
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title,
+      description,
+      images: [image],
+      creator: '@mysalimi'
+    },
+    icons,
+    metadataBase: new URL('https://scribe.salimi.my'),
+    themeColor: '#FFF',
+    ...(noIndex && {
+      robots: {
+        index: false,
+        follow: false
+      }
+    })
+  };
 }
