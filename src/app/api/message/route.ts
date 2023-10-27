7e83b0dbc2f9b30946bf40bcd7dca4376d4e0de9
Mohamad Salimi <contact@salimi.my>
2023-10-27 12:20:27 +0800

    Use edge for production

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -9,8 +9,9 @@ import { openai } from '@/lib/openai';
 import { pinecone } from '@/lib/pinecone';
 import { SendMessageValidator } from '@/lib/validators/SendMessageValidator';
 
-export const runtime =
-  process.env.NODE_ENV === 'production' ? 'edge' : 'nodejs';
+// export const runtime =
+//   process.env.NODE_ENV === 'production' ? 'edge' : 'nodejs';
+export const runtime = 'edge';
 
 export const POST = async (req: NextRequest) => {
   const body = await req.json();
